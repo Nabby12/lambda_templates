95674014224dd60a45e5612024aa331995297427
Nabby12 <Nabby12@users.noreply.github.com>
2022-03-31 15:34:45 +0900

    first-commit

---
+++ go/go-hello-world/app/go.mod
@@ -0,0 +1,10 @@
+module go-hello-world
+
+go 1.18
+
+require (
+	github.com/aws/aws-lambda-go v1.29.0
+	github.com/getsentry/sentry-go v0.13.0
+)
+
+require golang.org/x/sys v0.0.0-20211007075335-d3039528d8ac // indirect
---
+++ go/go-hello-world/app/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type MyEvent struct {
+	Name string `json:"name"`
+}
+
+func handler(ctx context.Context, name MyEvent) (string, error) {
+	now := time.Now()
+	fmt.Printf("-- Start job %s --\n", now.Format("2006-01-02 15:04:05"))
+
+	fmt.Printf("Hello %s!", name.Name)
+
+	fmt.Println("Time: ", time.Since(now).Milliseconds(), "ms")
+	fmt.Printf("-- Finish job %s --\n", now.Format("2006-01-02 15:04:05"))
+
+	return "exit function", nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
