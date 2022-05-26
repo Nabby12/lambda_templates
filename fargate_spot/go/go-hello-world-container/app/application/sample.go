d40d2ce50b8a9030ab5a0d7868e9c439a39e8bc3
Nabby12 <Nabby12@users.noreply.github.com>
2022-05-26 20:18:40 +0900

    feature/added-go-routine-func

--- fargate_spot/go/go-hello-world-container/app/application/sample.go
+++ fargate_spot/go/go-hello-world-container/app/application/sample.go
@@ -1,6 +1,8 @@
 package application
 
 import (
+	"fmt"
+	"sync"
 	"time"
 	// "app/domain"
 )
@@ -14,5 +16,43 @@ func Sample(second int) error {
 
 	// return err
 
+	// go routine で並列実行
+	loopItems := [...]string{"0", "1", "2"}
+	errChan := make(chan error, len(loopItems))
+	wg := new(sync.WaitGroup)
+	for _, item := range loopItems {
+		wg.Add(1)
+
+		item := item // go routine 起動よりも for ループ終了の方が早いため変数の代入が必要
+		go func(wg *sync.WaitGroup) {
+			defer wg.Done()
+
+			if err := SamplePrint(item); err != nil {
+				fmt.Printf("%v\n", err)
+				errChan <- err
+			}
+		}(wg)
+	}
+	wg.Wait()
+	close(errChan)
+
+	for err := range errChan {
+		if err != nil {
+			fmt.Printf("%v\n", err)
+			return err
+		}
+	}
+
+	return nil
+}
+
+func SamplePrint(item string) error {
+	fmt.Printf("pritn start: %s \n", time.Now().Format("2006-01-02 15:04:05"))
+	fmt.Printf("print item: %v\n", item)
+
+	// go routineで並列実行する処理
+	time.Sleep(time.Duration(2) * time.Second)
+
+	fmt.Printf("pritn end: %s \n", time.Now().Format("2006-01-02 15:04:05"))
 	return nil
 }
